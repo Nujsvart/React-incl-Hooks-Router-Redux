d42a3eda8f8723dca48e03f09f72d9879d351a78
Nujsvart <kampfar@gmail.com>
2022-02-01 23:52:10 +0300

    working with "refs"

--- 3) A Complete Practice Project/src/components/Button/Button.js
+++ 3) A Complete Practice Project/src/components/Button/Button.js
@@ -4,20 +4,19 @@ import "./button.css";
 const Button = props => {
   const handleSubmit = e => {
     e.preventDefault();
-    const age = props.user.userAge;
-    const name = props.user.userName;
-    if (name.trim().length === 0 || age.trim().length === 0) {
+    const userName = props.userName.current.value;
+    const userAge = props.userAge.current.value;
+    if (userName.trim().length === 0 || userAge.trim().length === 0) {
       props.showError("invalid input", "please enter a valid name");
       return;
     }
 
-    if (+age < 1) {
+    if (+userAge < 1) {
       props.showError("invalid age", "please enter a valid age");
       return;
     }
 
-    props.addUser(props.user);
-    props.resetForm();
+    props.addUser({ userName, userAge });
   };
 
   return (
--- 3) A Complete Practice Project/src/components/UserInput/UserInput.js
+++ 3) A Complete Practice Project/src/components/UserInput/UserInput.js
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../Button/Button";
 import Card from "../Card/Card";
 import "./userinput.css";
 
 const UserInput = props => {
-  const [userInfo, setUserInfo] = useState({ userName: "", userAge: "" });
+  const nameInputRef = useRef("");
+  const ageInputRef = useRef("");
 
+  /*   const [userInfo, setUserInfo] = useState({ userName: "", userAge: "" });
+  
   const handleUserName = e => {
     setUserInfo(prevState => ({
       ...prevState,
       userName: e.target.value,
     }));
+
   };
 
   const handleUserAge = e => {
@@ -23,6 +27,7 @@ const UserInput = props => {
   const resetForm = () => {
     setUserInfo({ userName: "", userAge: "" });
   };
+ */
 
   return (
     <div className="container">
@@ -33,19 +38,26 @@ const UserInput = props => {
             type="text"
             name="username"
             id="username"
-            value={userInfo.userName}
-            onChange={handleUserName}
+            //value={userInfo.userName}
+            //onChange={handleUserName}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             type="number"
             name="age"
             id="age"
-            value={userInfo.userAge}
-            onChange={handleUserAge}
+            //value={userInfo.userAge}
+            //onChange={handleUserAge}
+            ref={ageInputRef}
           />
 
-          <Button addUser={props.addUser} showError={props.showError} user={userInfo} resetForm={resetForm}>
+          <Button
+            addUser={props.addUser}
+            showError={props.showError}
+            userName={nameInputRef}
+            userAge={ageInputRef}
+          >
             Add User
           </Button>
         </form>
