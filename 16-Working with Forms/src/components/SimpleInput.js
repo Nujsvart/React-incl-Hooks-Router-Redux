874efb06f5c767517925b767b5211f02272c371c
Nujsvart <kampfar@gmail.com>
2022-03-16 14:44:15 +0300

    providing validation feedback

--- 16-Working with Forms/src/components/SimpleInput.js
+++ 16-Working with Forms/src/components/SimpleInput.js
@@ -1,8 +1,9 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const SimpleInput = props => {
   const [enteredName, setEnteredName] = useState("");
-  const nameInputRef = useRef();
+  const [enteredNameisValid, setEnteredNameIsValid] = useState(true);
+  //const nameInputRef = useRef();
 
   const nameInputChangeHandler = e => {
     setEnteredName(e.target.value);
@@ -11,22 +12,36 @@ const SimpleInput = props => {
   const formSubmissionHandler = e => {
     e.preventDefault();
 
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
     console.log(enteredName);
-    const enteredValue = nameInputRef.current.value;
-    console.log(enteredValue);
+    //const enteredValue = nameInputRef.current.value;
+    //console.log(enteredValue);
   };
+
+  const nameInputClasses = enteredNameisValid
+    ? "form-control"
+    : "form-control invalid";
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
-          ref={nameInputRef}
           onChange={nameInputChangeHandler}
           type="text"
           id="name"
           value={enteredName}
         />
+
+        {!enteredNameisValid && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
