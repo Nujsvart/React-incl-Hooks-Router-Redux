0daddc3c2e834d2eca59749f52fa2bf2d0858296
Nujsvart <kampfar@gmail.com>
2022-03-16 14:06:06 +0300

    adjusting the custom hook logic

--- 15x-A More Realistic Example/src/App.js
+++ 15x-A More Realistic Example/src/App.js
@@ -7,30 +7,26 @@ import useHttp from "./hooks/useHttp";
 function App() {
   const [tasks, setTasks] = useState();
 
-  const transformTasks = tasksObj => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp(
-    {
-      url: "https://react-deneme-6773c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-    },
-    transformTasks
-  );
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = tasksObj => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://react-deneme-6773c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+      },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const taskAddHandler = task => {
     setTasks([...tasks, task]);
