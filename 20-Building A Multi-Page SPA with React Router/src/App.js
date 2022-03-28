4d88ed128ae9caaa229c8832f00183fae74d94b3
Nujsvart <kampfar@gmail.com>
2022-03-29 01:21:03 +0300

    working with links

--- 20-Building A Multi-Page SPA with React Router/src/App.js
+++ 20-Building A Multi-Page SPA with React Router/src/App.js
@@ -1,14 +1,16 @@
+import { Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
-      <Router>
+      <MainHeader />
+      <main>
         <Route path="/welcome" component={Welcome} />
-        <Route path={ "/products"} component={Products} />
-      </Router>
+        <Route path="/products" component={Products} />
+      </main>
     </div>
   );
 }
