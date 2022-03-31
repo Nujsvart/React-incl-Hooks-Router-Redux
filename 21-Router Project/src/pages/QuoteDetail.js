ef4270dd7a56661d4cfa20a94f4d27dfd83f88b8
Nujsvart <kampfar@gmail.com>
2022-04-01 00:35:13 +0300

    practicing nested routes

--- 21-Router Project/src/pages/QuoteDetail.js
+++ 21-Router Project/src/pages/QuoteDetail.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
 
 const QuoteDetail = () => {
   const { quoteId } = useParams();
   return (
-    <div>
-      QuoteDetail
-      <h1>{quoteId}</h1>
-    </div>
+    <>
+      <h1>QuoteDetail</h1>
+      <p>{quoteId}</p>
+      <Route path={`/quotes/${quoteId}/comments`} component={Comments} />
+    </>
   );
 };
 
 export default QuoteDetail;
+
